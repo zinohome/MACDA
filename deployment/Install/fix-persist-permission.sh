2ff19ce047a1757f07baa30a3761e4bacca0bd32
zhangjun <ibmzhangjun@139.com>
2022-12-29 11:22:32 +0800

    update Dockerfile

--- deployment/Install/fix-persist-permission.sh
+++ deployment/Install/fix-persist-permission.sh
@@ -3,4 +3,5 @@ chown -R 1000:1000 persist/timescaledb/data
 chown -R 5050:5050 persist/pgadmin
 chown -R 1000:1000 persist/desktop/UserHome
 chown -R 1000:1000 persist/desktop/UserData
+chown -R 101:101 mock-data/redpanda/data
 
