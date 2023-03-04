1246df989c24921925254be54c639830b8b5e0d1
zhangjun <ibmzhangjun@139.com>
2023-03-04 19:49:15 +0800

    add faultreport

--- core/appSettings.py
+++ core/appSettings.py
@@ -46,6 +46,7 @@ class AppSettings(BaseSettings):
     APP_LOG_FILENAME: str = Field('macda.log', env='APP_LOG_FILENAME')
     TSDB_URL: str = Field('postgres://postgres:passw0rd@timescaledb:5432/postgres', env='TSDB_URL')
     TSDB_POOL_SIZE: int = Field(20, env='TSDB_POOL_SIZE')
+    TSDB_PARSE_BATCH_TIME: int = Field(10, env='TSDB_PARSE_BATCH_TIME')
     TSDB_BATCH_TIME: int = Field(10, env='TSDB_BATCH_TIME')
     #Rest Server URL
     SEND_FAULT_INTERVAL: int = Field(300, env='SEND_FAULT_INTERVAL')
--- pipeline/batchstore/agents.py
+++ pipeline/batchstore/agents.py
@@ -12,12 +12,14 @@ from app import app
 from core.settings import settings
 from pipeline.batchstore.models import input_topic
 from utils.log import log as log
+from utils.sensorpolyfit import SensorPolyfit
 from utils.tsutil import TSutil
 
 
 @app.agent(input_topic)
 async def store_signal(stream):
     tu = TSutil()
+    sp = SensorPolyfit()
     async for datas in stream.take(10000, within=settings.TSDB_BATCH_TIME):
         log.debug("==================== Get parsed data batch ====================")
         dev_mode = settings.DEV_MODE
@@ -28,6 +30,72 @@ async def store_signal(stream):
             tu.batchinsert('pro_macda', 'msg_calc_dvc_time', datas)
             #tu.batchinsertjson('pro_macda_json', 'msg_calc_dvc_time', datas)
         log.debug("Saved data with batch length: %s" % len(datas))
+
+        # Predict
+        for data in datas:
+            log.debug("==========---------- Get predict data in batch ==========----------")
+            dev_mode = settings.DEV_MODE
+            mode = 'dev'
+            if not dev_mode:
+                mode = 'pro'
+            predictdata = tu.get_predictdata(mode, data['payload']['msg_calc_dvc_no'])
+            if predictdata['len'] > 0:
+                # ref leak predict
+                ref_leak_u11 = sp.polyfit(predictdata['data']['dvc_w_op_mode_u1'], predictdata['data']['dvc_i_fat_u1'],
+                                          predictdata['data']['dvc_w_freq_u11'],
+                                          predictdata['data']['dvc_i_suck_pres_u11'])
+                ref_leak_u12 = sp.polyfit(predictdata['data']['dvc_w_op_mode_u1'], predictdata['data']['dvc_i_fat_u1'],
+                                          predictdata['data']['dvc_w_freq_u12'],
+                                          predictdata['data']['dvc_i_suck_pres_u12'])
+                ref_leak_u21 = sp.polyfit(predictdata['data']['dvc_w_op_mode_u2'], predictdata['data']['dvc_i_fat_u2'],
+                                          predictdata['data']['dvc_w_freq_u21'],
+                                          predictdata['data']['dvc_i_suck_pres_u21'])
+                ref_leak_u22 = sp.polyfit(predictdata['data']['dvc_w_op_mode_u2'], predictdata['data']['dvc_i_fat_u2'],
+                                          predictdata['data']['dvc_w_freq_u22'],
+                                          predictdata['data']['dvc_i_suck_pres_u22'])
+                # ref pump predict
+                ref_pump_u1 = 0
+                ref_pump_u2 = 0
+                if predictdata['data']['w_frequ1_sub'] < 1 and predictdata['data']['w_crntu1_sub'] > 2:
+                    ref_pump_u1 = 1
+                if predictdata['data']['w_frequ2_sub'] < 1 and predictdata['data']['w_crntu2_sub'] > 2:
+                    ref_pump_u2 = 1
+                # sensor predict
+                fat_sensor = 0
+                rat_sensor = 0
+                if predictdata['data']['dvc_bflt_trainmove'] > 0.8 and predictdata['data']['fat_sub'] > 8:
+                    fat_sensor = 1
+                if predictdata['data']['dvc_bflt_trainmove'] > 0.8 and predictdata['data']['rat_sub'] > 8:
+                    rat_sensor = 1
+                cabin_overtemp = 0
+                if predictdata['data']['dvc_bflt_cabinovertemp'] > 0.8:
+                    cabin_overtemp = 1
+                if ref_leak_u11 + ref_leak_u12 + ref_leak_u21 + ref_leak_u22 + ref_pump_u1 + ref_pump_u2 + fat_sensor + rat_sensor + cabin_overtemp > 0:
+                    # write to db
+                    if dev_mode:
+                        key = f"{data['payload']['msg_calc_dvc_no']}-{data['payload']['msg_calc_parse_time']}"
+                    else:
+                        key = f"{data['payload']['msg_calc_dvc_no']}-{data['payload']['msg_calc_dvc_time']}"
+                    log.debug("Add predict data  with key : %s" % key)
+                    predictjson = {}
+                    predictjson['msg_calc_dvc_time'] = data['payload']['msg_calc_dvc_time']
+                    predictjson['msg_calc_parse_time'] = data['payload']['msg_calc_parse_time']
+                    predictjson['msg_calc_dvc_no'] = data['payload']['msg_calc_dvc_no']
+                    predictjson['msg_calc_train_no'] = data['payload']['msg_calc_train_no']
+                    predictjson['ref_leak_u11'] = ref_leak_u11
+                    predictjson['ref_leak_u12'] = ref_leak_u12
+                    predictjson['ref_leak_u21'] = ref_leak_u21
+                    predictjson['ref_leak_u22'] = ref_leak_u22
+                    predictjson['ref_pump_u1'] = ref_pump_u1
+                    predictjson['ref_pump_u2'] = ref_pump_u2
+                    predictjson['fat_sensor'] = fat_sensor
+                    predictjson['rat_sensor'] = rat_sensor
+                    predictjson['cabin_overtemp'] = cabin_overtemp
+                    if dev_mode:
+                        tu.insert_predictdata('dev_predict', predictjson)
+                    else:
+                        tu.insert_predictdata('pro_predict', predictjson)
+
 
     '''
     async for data in stream:
