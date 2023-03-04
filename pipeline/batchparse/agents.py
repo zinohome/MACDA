e669306ba84b31e0861678e00a3a16b3033b705f
zhangjun <ibmzhangjun@139.com>
2023-03-04 20:11:03 +0800

    add faultreport

--- pipeline/batchparse/agents.py
+++ pipeline/batchparse/agents.py
@@ -47,16 +47,17 @@ async def parse_signal(stream):
                 if 'message_data' in datadict.keys():
                     # Parse data and send to parsed topic
                     parsed_dict = Nb5.from_bytes_to_dict(binascii.a2b_hex(datadict['message_data']))
-                    out_record = {"schema":jschema,"payload":parsed_dict}
-                    if dev_mode:
-                        key = f"{parsed_dict['msg_calc_dvc_no']}-{parsed_dict['msg_calc_parse_time']}"
-                    else:
-                        key = f"{parsed_dict['msg_calc_dvc_no']}-{parsed_dict['msg_calc_dvc_time']}"
-                    log.debug("---------- Parsed data with key : %s" % f"{parsed_dict['msg_calc_dvc_no']}-{parsed_dict['msg_calc_dvc_time']}")
-                    await output_topic.send(key=key, value=out_record, schema=output_schema)
-                    # Send json to Archive topics
-                    archivetopicname = f"MACDA-archive-{settings.PARSED_TOPIC_NAME}-{parsed_dict['msg_calc_dvc_no']}"
-                    archivetopic = app.topic(archivetopicname, partitions=settings.TOPIC_PARTITIONS, value_serializer='json')
-                    await archivetopic.send(key=key,value=out_record)
+                    if not parsed_dict['msg_calc_dvc_no'].strip().startswith('2059-'):
+                        out_record = {"schema":jschema,"payload":parsed_dict}
+                        if dev_mode:
+                            key = f"{parsed_dict['msg_calc_dvc_no']}-{parsed_dict['msg_calc_parse_time']}"
+                        else:
+                            key = f"{parsed_dict['msg_calc_dvc_no']}-{parsed_dict['msg_calc_dvc_time']}"
+                        log.debug("---------- Parsed data with key : %s" % f"{parsed_dict['msg_calc_dvc_no']}-{parsed_dict['msg_calc_dvc_time']}")
+                        await output_topic.send(key=key, value=out_record, schema=output_schema)
+                        # Send json to Archive topics
+                        archivetopicname = f"MACDA-archive-{settings.PARSED_TOPIC_NAME}-{parsed_dict['msg_calc_dvc_no']}"
+                        archivetopic = app.topic(archivetopicname, partitions=settings.TOPIC_PARTITIONS, value_serializer='json')
+                        await archivetopic.send(key=key,value=out_record)
                 else:
                     log.debug("-------------------- No message data --------------------")
